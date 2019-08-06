b3dcdf71f5be70bf9c7071e3b04d3e771170f49b
anderson.bellini <anderson.bellini@anac.gov.br>
2019-08-06 13:15:49 -0300

    Passar parametro em querystring node

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const server = express();
 
 server.get("/", (req, res) => {
-  return res.send("Hello World");
+  return res.send(`Hello ${req.query.name}`);
 });
 
 server.listen(3333);
