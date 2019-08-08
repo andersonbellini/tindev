8ccde50aa6c596d3329d00491c1725af0cf0e3d6
anderson.bellini <anderson.bellini@anac.gov.br>
2019-08-08 11:08:32 -0300

    criação de StyleSheet

--- tindev/App.js
+++ tindev/App.js
@@ -1,6 +1,28 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
 import React from "react";
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
-  return <Text>Hello World </Text>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>Hello World </Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: "#7159c1",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  texto: {
+    fontWeight: "bold",
+    color: "#FFF",
+    fontSize: 20
+  }
+});
