23e30bc103224a86340fac3c16d86796928c80cf
anderson.bellini <anderson.bellini@anac.gov.br>
2019-08-13 07:52:02 -0300

    Finalização app

--- tindev/src/index.js
+++ tindev/src/index.js
@@ -1,6 +1,14 @@
-import React from "react";
-import Routes from "./routes";
+import React from 'react';
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket'
+]);
+
+import Routes from './routes';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <Routes />
+  );
 }
--- tindev/src/pages/Login.js
+++ tindev/src/pages/Login.js
@@ -1,47 +1,41 @@
-import React, { useState, useEffect } from "react";
-import AsyncStorage from "@react-native-community/async-storage /";
-import {
-  KeyboardAvoidingView,
-  Platform,
-  Text,
-  StyleSheet,
-  Image,
-  TextInput,
-  TouchableOpacity
-} from "react-native";
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage'
+import { KeyboardAvoidingView, Platform, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
 
-import api from "../services/api";
+import api from '../services/api';
 
-import logo from "../assets/logo.png";
+import logo from '../assets/logo.png';
 
 export default function Login({ navigation }) {
-  const [user, setUser] = useState("");
+  const [user, setUser] = useState('');
 
   useEffect(() => {
-    AsyncStorage.getItem("user").then(user => {
+    AsyncStorage.getItem('user').then(user => {
       if (user) {
-        navigation.navigate("Main", { user });
+        navigation.navigate('Main', { user })
       }
-    });
-  }, [navigation]);
+    })
+  }, []);
 
-  async function hadleLogin() {
-    const response = await api.post("/devs", { username: user });
+  async function handleLogin() {
+    const response = await api.post('/devs', { username: user });
+
     const { _id } = response.data;
-    await AsyncStorage.setItem("user", _id);
 
-    navigation.navigate("Main", { _id });
+    await AsyncStorage.setItem('user', _id);
+
+    navigation.navigate('Main', { user: _id });
   }
 
   return (
-    <KeyboardAvoidingView
+    <KeyboardAvoidingView 
       behavior="padding"
-      enabled={Platform.OS === "ios"}
+      enabled={Platform.OS === 'ios'}
       style={styles.container}
     >
       <Image source={logo} />
 
-      <TextInput
+      <TextInput 
         autoCapitalize="none"
         autoCorrect={false}
         placeholder="Digite seu usuário no Github"
@@ -51,7 +45,7 @@ export default function Login({ navigation }) {
         onChangeText={setUser}
       />
 
-      <TouchableOpacity onPress={hadleLogin} style={styles.button}>
+      <TouchableOpacity onPress={handleLogin} style={styles.button}>
         <Text style={styles.buttonText}>Enviar</Text>
       </TouchableOpacity>
     </KeyboardAvoidingView>
@@ -61,33 +55,36 @@ export default function Login({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f5f5f5",
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: '#f5f5f5',
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 30
   },
+
   input: {
     height: 46,
-    alignSelf: "stretch",
-    backgroundColor: "#FFF",
+    alignSelf: 'stretch',
+    backgroundColor: '#FFF',
     borderWidth: 1,
-    borderColor: "#DDD",
+    borderColor: '#ddd',
     borderRadius: 4,
     marginTop: 20,
-    paddingHorizontal: 15
+    paddingHorizontal: 15,
   },
+
   button: {
     height: 46,
-    alignSelf: "stretch",
-    backgroundColor: "#DF4723",
-    borderBottomColor: 4,
+    alignSelf: 'stretch',
+    backgroundColor: '#DF4723',
+    borderRadius: 4,
     marginTop: 10,
-    justifyContent: "center",
-    alignItems: "center"
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  buttontext: {
-    color: "#FFF",
-    fontWeight: "bold",
-    fontSize: 16
-  }
+
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
 });
